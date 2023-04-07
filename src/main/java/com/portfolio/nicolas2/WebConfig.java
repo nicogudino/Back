124e139518c805b3568111c169f1ba6bc5aa7903
Nicolas <nicolas_gudino@hotmail.com>
2023-04-07 01:13:04 -0300

    prueba

--- src/main/java/com/portfolio/nicolas2/WebConfig.java
+++ src/main/java/com/portfolio/nicolas2/WebConfig.java
@@ -11,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("https://frontendproyecto-ff35c.web.app/");
+        registry.addMapping("/**");
     }
 }
